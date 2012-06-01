454720adab62b7d72c82b645e4f2d518020ff436
Ivar Vong <radiodog@gmail.com>
2012-06-01 21:17:04 +0000

    now it's a quote generator...

--- twilio.js
+++ twilio.js
@@ -1,9 +1,13 @@
 var sys = require('sys')
+  , fs = require('fs')
   , TwilioClient = require('twilio').Client
   , Twim1 = require('twilio').Twim1
   , credentials = require('./credentials.js')
   , client = new TwilioClient(credentials.sid, credentials.authToken, credentials.hostname)
   , phone = client.getPhoneNumber(credentials.phoneNumber)
+
+var quotes = fs.readFileSync('quotes').toString().split("\n");
+console.log("Loaded", quotes.length, "quotes...");
 
 phone.setup(function() {
   phone.on('incomingSms', function(reqParams, res) {
@@ -11,7 +15,9 @@ phone.setup(function() {
     console.log('recevied', reqParams.Body);
     console.log('from', reqParams.From);
 
-    var message = "Hi! You said: '" + reqParams.Body + "' <3, The Emerald";
+    //var message = "Hi! You said: '" + reqParams.Body + "' <3, The Emerald";
+
+    var message = quotes[ parseInt(Math.random()*(quotes.length-2)) ];
 
     phone.sendSms(reqParams.From, message, null, function(sms) {
       sms.on('processed', function(reqParams, response) {
